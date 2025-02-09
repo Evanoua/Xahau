06931c74bbf10333ea1cb9f1505b688ec0fa2628
Ev4Nou4 <shaynetrudel@hotmail.com>
2025-02-08 21:28:46 -0500

    Create MetamaskIntegration.js

---
+++ MetamaskIntegration.js
@@ -0,0 +1,17 @@
+let provider, signer;
+
+async function connectWallet() {
+    if (!window.ethereum) {
+        alert("MetaMask n'est pas installé !");
+        return;
+    }
+
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+
+    const address = await signer.getAddress();
+    document.getElementById("wallet").innerText = "Connecté : " + address;
+}
+
+document.getElementById("connect").addEventListener("click", connectWallet);
